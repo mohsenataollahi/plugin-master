2f051a59fab6d071cd1dc79d38be56dd607ebdee
mohsen <mohsenataollahi@yahoo.com>
2024-02-15 21:55:34 +0330

    add functions to create the table at install

--- plugin.php
+++ plugin.php
@@ -64,7 +64,15 @@ class ExamplePluginInit extends Singleton
              */
             $this->application->onActivation(function () {
                 // Create tables or something else
-
+	            global $wpdb;
+	            $table_name = $wpdb->prefix . 'books_info';
+	            $table_exists = $wpdb->get_var("SHOW TABLES LIKE '$table_name'") === $table_name;
+	            if (!$table_exists) {
+		            $charset_collate = $wpdb->get_charset_collate();
+		            $sql = "CREATE TABLE {$table_name} ( ID mediumint(9) NOT NULL AUTO_INCREMENT, post_id mediumint(9) NOT NULL, isbn varchar(255) NOT NULL, PRIMARY KEY  (ID) ) $charset_collate;";
+		            require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		            dbDelta( $sql );
+	            }
             });
 
             /**
