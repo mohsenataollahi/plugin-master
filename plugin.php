0e17717911c1d38e6794c6d2c7209a74ffa27de5
mohsen <mohsenataollahi@yahoo.com>
2024-02-16 17:00:37 +0330

    modify plugin.php to add table at activation

--- plugin.php
+++ plugin.php
@@ -22,101 +22,107 @@ use Rabbit\Utils\Singleton;
 use League\Container\Container;
 use src\ConfigsBookStore;
 
-if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
-    require dirname(__FILE__) . '/vendor/autoload.php';
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
 /**
  * Class ExamplePluginInit
  * @package ExamplePluginInit
  */
-class ExamplePluginInit extends Singleton
-{
-    /**
-     * @var Container
-     */
-    private $application;
+class ExamplePluginInit extends Singleton {
+	/**
+	 * @var Container
+	 */
+	private $application;
 
-    /**
-     * ExamplePluginInit constructor.
-     */
-    public function __construct()
-    {
-        $this->application = Application::get()->loadPlugin(__DIR__, __FILE__, 'config');
+	/**
+	 * ExamplePluginInit constructor.
+	 */
+	public function __construct() {
+		$this->application = Application::get()->loadPlugin( __DIR__, __FILE__, 'config' );
 
-        $this->init();
-    }
+		$this->init();
+	}
 
-    public function init()
-    {
-        try {
+	public function init() {
+		try {
 
-            /**
-             * Load service providers
-             */
-            $this->application->addServiceProvider(RedirectServiceProvider::class);
-            $this->application->addServiceProvider(DatabaseServiceProvider::class);
-            $this->application->addServiceProvider(TemplatesServiceProvider::class);
-            $this->application->addServiceProvider(LoggerServiceProvider::class);
-            // Load your own service providers here...
+			/**
+			 * Load service providers
+			 */
+			$this->application->addServiceProvider( RedirectServiceProvider::class );
+			$this->application->addServiceProvider( DatabaseServiceProvider::class );
+			$this->application->addServiceProvider( TemplatesServiceProvider::class );
+			$this->application->addServiceProvider( LoggerServiceProvider::class );
+			// Load your own service providers here...
 
 
-            /**
-             * Activation hooks
-             */
-            $this->application->onActivation(function () {
-                // Create tables or something else
+			/**
+			 * Activation hooks
+			 */
+			$this->application->onActivation( function () {
+				// Create tables or something else
 
-	            // TODO: add function to create my table
-//	            $db = ConfigsBookStore();
-//	            $db->createBooksTable();
-            });
+				/**
+				 * Create Table books_info in DB.
+				 */
+				global $wpdb;
+				$db           = $wpdb;
+				$Table        = $db->prefix . 'books_info';
+				$table_exists = $db->get_var( "SHOW TABLES LIKE '$Table'" ) === $Table;
+				if ( ! $table_exists ) {
+					$charset_collate = $db->get_charset_collate();
+					$sql             = "CREATE TABLE {$Table} ( ID mediumint(9) NOT NULL AUTO_INCREMENT, post_id mediumint(9) NOT NULL, isbn varchar(255) NOT NULL, PRIMARY KEY  (ID) ) $charset_collate;";
+					require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+					dbDelta( $sql );
+				}
+			} );
 
-            /**
-             * Deactivation hooks
-             */
-            $this->application->onDeactivation(function () {
-                // Clear events, cache or something else
+			/**
+			 * Deactivation hooks
+			 */
+			$this->application->onDeactivation( function () {
+				// Clear events, cache or something else
 
-            });
+			} );
 
-            $this->application->boot(function (Plugin $plugin) {
-                $plugin->loadPluginTextDomain();
+			$this->application->boot( function ( Plugin $plugin ) {
+				$plugin->loadPluginTextDomain();
 
-                // load template
-                $this->application->template('plugin-template.php', ['foo' => 'bar']);
+				// load template
+				$this->application->template( 'plugin-template.php', [ 'foo' => 'bar' ] );
 
-	            /// load bookstore main files
-	            $plugin->includes( __DIR__ . '/src');
+				/// load bookstore main files
+				$plugin->includes( __DIR__ . '/src' );
 
-            });
+			} );
 
-        } catch (Exception $e) {
-            /**
-             * Print the exception message to admin notice area
-             */
-            add_action('admin_notices', function () use ($e) {
-                AdminNotice::permanent(['type' => 'error', 'message' => $e->getMessage()]);
-            });
+		} catch ( Exception $e ) {
+			/**
+			 * Print the exception message to admin notice area
+			 */
+			add_action( 'admin_notices', function () use ( $e ) {
+				AdminNotice::permanent( [ 'type' => 'error', 'message' => $e->getMessage() ] );
+			} );
 
-            /**
-             * Log the exception to file
-             */
-            add_action('init', function () use ($e) {
-                if ($this->application->has('logger')) {
-                    $this->application->get('logger')->warning($e->getMessage());
-                }
-            });
-        }
-    }
+			/**
+			 * Log the exception to file
+			 */
+			add_action( 'init', function () use ( $e ) {
+				if ( $this->application->has( 'logger' ) ) {
+					$this->application->get( 'logger' )->warning( $e->getMessage() );
+				}
+			} );
+		}
+	}
 
-    /**
-     * @return Container
-     */
-    public function getApplication()
-    {
-        return $this->application;
-    }
+	/**
+	 * @return Container
+	 */
+	public function getApplication() {
+		return $this->application;
+	}
 }
 
 /**
@@ -124,9 +130,8 @@ class ExamplePluginInit extends Singleton
  *
  * @return ExamplePluginInit
  */
-function examplePlugin()
-{
-    return ExamplePluginInit::get();
+function examplePlugin() {
+	return ExamplePluginInit::get();
 }
 
 examplePlugin();
